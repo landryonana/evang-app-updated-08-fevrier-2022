b5130b4020e6fcec769836d9f7fe6dddbf05e0d9
landryonana <51939280+landryonana@users.noreply.github.com>
2022-02-08 20:25:12 +0100

    Update admin.py

--- evangelisation/admin.py
+++ evangelisation/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 from evangelisation.models import Evangelisation,  Person, Site, Suivi, Image, Participant, Profile
 
 
+class Userresource(resources.ModelResource):
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'first_name', 'last_name', 'email', 
+				'date_joined', 'is_active', 'is_staff', 
+				'is_superuser', 'last_login', 'password')
 
+class UserAdmin(ImportExportModelAdmin):
+	list_display = ('id', 'username', 'first_name', 'email')
+	resource_class = Userresource
 
 class SiteAdmin(ImportExportModelAdmin):
 	pass
@@ -32,6 +43,9 @@ class ProfileAdmin(ImportExportModelAdmin):
 class ImageAdmin(ImportExportModelAdmin):
 	pass
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Site, SiteAdmin)
 admin.site.register(Person, PersonAdmin)
@@ -39,6 +53,7 @@ admin.site.register(Evangelisation, EvangelisationAdmin)
 admin.site.register(Suivi, SuiviAdmin)
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Image, ImageAdmin)
+
 
 
 
